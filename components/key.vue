337f2b11085540055f7bf81820eab822471303cd
betaro-vlab <director.vlab@gmail.com>
2019-11-17 11:12:24 +0900

    テスト用のキーを作った

---
+++ components/key.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-card @mousedown="toneTest()">test</v-card>
+</template>
+
+<script>
+export default {
+  name: 'Key',
+  props: {
+    synth: {
+      type: Object
+    }
+  },
+  data() {
+    return {}
+  },
+  methods: {
+    toneTest() {
+      this.synth.triggerAttackRelease('C4', '8n')
+    }
+  }
+}
+</script>
+
+<style></style>
