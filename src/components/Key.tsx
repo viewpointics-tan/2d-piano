529e174fb4460af7f6a9b55f2a21652c30f8bea6
viewpointicstan <viewpointicstan@gmail.com>
2021-08-05 16:13:56 +0900

    update: pianoKeyDown color

--- src/components/Key.tsx
+++ src/components/Key.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import * as math from '../util/math'
 
 interface Props {
@@ -36,17 +36,34 @@ const Key = ({
     width: '95%',
     height: '90%',
   }
+
+  const [pianoKeyDown, setPianoKeyDown] = useState(false)
+  const onPianoKeyDown = () => {
+    setPianoKeyDown(true)
+  }
+  const onPianoKeyUp = () => {
+    setPianoKeyDown(false)
+  }
+  const colorClassName = () => {
+    if (pianoKeyDown) {
+      return 'bg-red-300'
+    }
+    if (parity) {
+      return 'bg-white'
+    }
+    return 'bg-yellow-200'
+  }
+
   return (
     <div style={boxStyle}>
       <div
-        className={`flex-shrink-0 rounded-lg h-full shadow-lg flex justify-evenly items-center ${
-          parity ? 'bg-white' : 'bg-yellow-200'
-        }`}
+        className={`flex-shrink-0 rounded-lg h-full shadow-lg flex justify-evenly items-center ${colorClassName()}`}
         style={keyStyle}
+        onPointerDown={onPianoKeyDown}
+        onPointerUp={onPianoKeyUp}
+        onPointerOut={onPianoKeyUp}
       >
-        <div className="text-2xl font-serif">
-          {keyNumber}
-        </div>
+        <div className="text-2xl font-serif">{keyNumber}</div>
         <div className="text-3xl font-serif">
           {noteName[math.mod(keyNumber, 12)]}
         </div>
