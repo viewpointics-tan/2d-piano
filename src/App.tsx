0dd19130614ff861230139dc612e802dd056c44e
viewpointicstan <viewpointicstan@gmail.com>
2021-08-05 19:24:24 +0900

    update: define playSound

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import SoundFont from 'soundfont-player'
 import Row from './components/Row'
 
 const rowNumber = 5
@@ -8,7 +9,29 @@ const keyWidth = 40
 const deltaX = 2
 const deltaY = 3
 
+const ac = new AudioContext()
+
 const App = (): JSX.Element => {
+  const playSound = {
+    noteOn: (keyNumber: number) =>
+      SoundFont.instrument(ac, 'acoustic_grand_piano')
+        .then((inst) => inst.play(`${keyNumber}`, ac.currentTime, { gain: 20 }))
+        .catch((err) => {
+          console.log(err)
+        }),
+    noteOff: (instPromise: Promise<void | SoundFont.Player>) => {
+      instPromise
+        .then((player) => {
+          if (typeof player === 'object') {
+            player.stop()
+          }
+        })
+        .catch((err) => {
+          console.log(err)
+        })
+    },
+  }
+
   const rows = [...Array(rowNumber).keys()].map((index) => (
     <Row
       rowNumber={rowNumber}
